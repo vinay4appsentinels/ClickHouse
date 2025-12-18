943daa785ef9f45fc5132c32007e8c976bc9b009
Bharat Nallan Chakravarthy <bharat@clickhouse.com>
2025-12-18 19:14:46 +0530

    fix possible crash with filesystem_cache_boundary_alignment

--- src/Interpreters/Cache/FileCacheUtils.h
+++ src/Interpreters/Cache/FileCacheUtils.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <Core/Types.h>
 
 namespace FileCacheUtils
 {
@@ -15,6 +14,15 @@ static size_t roundUpToMultiple(size_t num, size_t multiple)
 {
     if (!multiple)
         return num;
+
+    /// Check for potential overflow in: num + multiple - 1. This happens when
+    /// num + multiple - 1 > SIZE_MAX (max value of size_t)
+    if (num > SIZE_MAX - multiple + 1)
+    {
+        /// Return SIZE_MAX as we can't round up further.
+        return SIZE_MAX;
+    }
+
     return roundDownToMultiple(num + multiple - 1, multiple);
 }
 
